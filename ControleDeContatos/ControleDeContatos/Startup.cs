1eb204f3f9b0a743f0eda21ee3fbca87562b7f46
Carlos Eduardo <carlosedx.alm@gmail.com>
2023-06-04 14:41:34 -0300

    criação do sistema de cadastro de usuários no banco de dados, criação da listagem de usuários, criação da migration.

---
+++ ControleDeContatos/ControleDeContatos/Startup.cs
@@ -0,0 +1,39 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Repositorio;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) 
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            IServiceCollection serviceCollection = services.AddEntityFrameworkSqlServer()
+                .AddDbContext<BancoContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DataBase")));
+            services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment) 
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
